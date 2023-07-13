4605cf90cb380f7536f50661d30dbd2d652c82d2
masahiro331 <m_fujimura@r.recruit.co.jp>
2023-07-13 12:47:56 +0900

    fix(redhat): patch fetch redhat oval v1 to use local oval data

--- fetcher/redhat/redhat.go
+++ fetcher/redhat/redhat.go
@@ -2,6 +2,7 @@ package redhat
 
 import (
 	"fmt"
+	"os"
 	"strconv"
 
 	"github.com/inconshreveable/log15"
@@ -20,23 +21,22 @@ func FetchFiles(versions []string) (map[string][]util.FetchResult, error) {
 			continue
 		}
 
-		if n < 5 {
+		result, err := fetchFilesFromLocal(v)
+		if err != nil {
+			return nil, xerrors.Errorf("Failed to read. err: %w", err)
+		}
+		results[v] = append(results[v], result)
+
+		if n < 6 {
 			log15.Warn("Skip redhat because no vulnerability information provided.", "version", v)
 			continue
 		}
 
 		reqs := []util.FetchRequest{{
 			Target:   v,
-			URL:      fmt.Sprintf("https://www.redhat.com/security/data/oval/com.redhat.rhsa-RHEL%s.xml.bz2", v),
+			URL:      fmt.Sprintf("https://access.redhat.com/security/data/oval/v2/RHEL%s/rhel-%s.oval.xml.bz2", v, v),
 			MIMEType: util.MIMETypeBzip2,
 		}}
-		if n != 5 {
-			reqs = append(reqs, util.FetchRequest{
-				Target:   v,
-				URL:      fmt.Sprintf("https://access.redhat.com/security/data/oval/v2/RHEL%s/rhel-%s.oval.xml.bz2", v, v),
-				MIMEType: util.MIMETypeBzip2,
-			})
-		}
 
 		rs, err := util.FetchFeedFiles(reqs)
 		if err != nil {
@@ -48,4 +48,18 @@ func FetchFiles(versions []string) (map[string][]util.FetchResult, error) {
 		return nil, xerrors.New("There are no versions to fetch")
 	}
 	return results, nil
+}
+
+func fetchFilesFromLocal(version string) (util.FetchResult, error) {
+	buf, err := os.ReadFile(fmt.Sprintf("data/oval/rhel/com.redhat.rhsa-RHEL%s.xml", version))
+	if err != nil {
+		return util.FetchResult{}, err
+	}
+
+	result := util.FetchResult{
+		Body: buf,
+		URL:  fmt.Sprintf("https://www.redhat.com/security/data/oval/com.redhat.rhsa-RHEL%s.xml.bz2", version),
+	}
+
+	return result, nil
 }
